74b9eba8cf29e9cab33b995a145448a6f4cbbe8d
kimkibum <shj000812@gmail.com>
2023-05-02 16:31:01 +0900

    ttt

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
 function App() {
   return <div className="App">Hello React!!</div>;
-=======
-import './App.css';
-
-function App () {
-  return (
-  <div className="App">
-    Hello React!!
-  </div>
-  );
->>>>>>> c1106a721e0f4856c67c536b24c04bfbe9edfb4b
 }
 
 export default App;
