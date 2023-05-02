9baa8b07b59523e3988f5f59c4b2ee0f246dd659
thunderjh <wlgus3908@naver.com>
2023-05-02 17:14:07 +0900

    커밋

--- src/main/header.js
+++ src/main/header.js
@@ -1,16 +1,17 @@
 import React from 'react';
-
+import mhholdingsLogo from '../img/mhholdingsLogo.png';
+import '../css/main.css';
 const Header = () => {
   return (
     <>
     <div className="h_wrap">
-      <h1 className="logo"><a data-menuanchor="MH HOLDINGS" href="#MH HOLDINGS"><img src="<%=path_main%>/img/main/mhholdingsLogo.png" /></a></h1>
-      <span id="m_buttom" onclick="m_button_click();"></span>
+      <h1 className="logo"><a><img src={mhholdingsLogo} /></a></h1>
+      <span id="m_buttom"></span>
       <nav id="menu">
-        <a className="menu-links" data-menuanchor="BUSINESS" href="#BUSINESS">BUSINESS</a>
-        <a className="menu-links" data-menuanchor="HEALTH COLUMN" href="#HEALTH COLUMN">HEALTH COLUMN</a>
-        <a className="menu-links" data-menuanchor="COMPANY" href="#COMPANY">COMPANY</a>
-        <a className="menu-links" data-menuanchor="CONTACT US" href="#CONTACT US">CONTACT US</a>
+        <a className="menu-links">BUSINESS</a>
+        <a className="menu-links">HEALTH COLUMN</a>
+        <a className="menu-links">COMPANY</a>
+        <a className="menu-links">CONTACT US</a>
       </nav>
     </div>
     </>
