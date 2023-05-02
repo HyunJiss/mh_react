f15164ec5f15a314c2ce06b2cfb2c6f8ad951354
HyunJi <shj000812@gmail.com>
2023-05-02 17:13:02 +0900

    app.js 수정 (오류)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Header from './main/header';
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import header from '.main/header.js'
 
-function App () {
-  return (
-  <div className="App">
-    <Header/>
-  </div>
-  );
-
+function App() {
+  return {
+    <> 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/main" element=(<header />) />
+        </Routes>
+      </BrowserRouter>
+    </>
+  }
+     
 }
+
 export default App;
