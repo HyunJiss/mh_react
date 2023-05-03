26a21f1e404c862000d213792102bf086412d64e
kimkibum <shj000812@gmail.com>
2023-05-03 16:03:53 +0900

    name modify

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./main/Header.js";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Header/>
+        <Header />
         <Routes>
           <Route path="/main" element={<Header />} />
-          <Route path="/main1" element={<Header />} />
-          <Route path="/main2" element={<Header />} />
-          <Route path="/main3" element={<Header />} />
         </Routes>
       </BrowserRouter>
     </>
   );
-     
 }
 
 export default App;
