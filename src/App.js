9cd1da34b86fa0da7b6d73bd7a17d3c4e117f71f
HyunJi <shj000812@gmail.com>
2023-05-03 16:13:30 +0900

    app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "./main/Header.js";
+import Main from "./main/Main.js";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
+        <Header/>
+        <Main/>
         <Routes>
           <Route path="/main" element={<Header />} />
+          <Route path="/main1" element={<Main />} />
         </Routes>
       </BrowserRouter>
     </>
   );
+     
 }
 
 export default App;
