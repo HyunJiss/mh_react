52ad325205df3999e8aa026d26a59a21f8e2ce5d
thunderjh <wlgus3908@naver.com>
2023-05-03 09:54:25 +0900

    z

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Header from "./main/header.js";
+import Header from "./main/Header.js";
 
 function App() {
   return (
