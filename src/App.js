7a60b773a8dce7c5bc56562041493319fe5421b7
thunderjh <wlgus3908@naver.com>
2023-05-03 17:09:59 +0900

    푸터

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "./main/Header.js";
 import Main from "./main/Main.js";
+import Footer from "./main/Footer.js";
 
 function App() {
   return (
     <>
-      <Main/>
       <BrowserRouter>
         <Header/>
         <Main/>
@@ -14,6 +14,8 @@ function App() {
           <Route path="/main" element={<Header />} />
         </Routes>
       </BrowserRouter>
+      <Main/>
+      <Footer/>
     </>
   );
      
